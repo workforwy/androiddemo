a9e0a0835b862382a101d77526b8df50aef07b9e
wy <yong.wang@medbanks.cn>
2019-12-24 14:06:42 +0800

    拍照优化

--- app/src/main/java/com/ceshi/ha/activity/TakePhotoActivity.java
+++ app/src/main/java/com/ceshi/ha/activity/TakePhotoActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
@@ -19,12 +20,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.ceshi.ha.R;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.security.Provider;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -33,29 +36,32 @@ import java.util.Locale;
 
 public class TakePhotoActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_TAKE_PHOTO = 0x110;
-    private String mCurrentPhotoPath;
+    public static final int PHOTO_REQUEST_CAREMA = 1;// 拍照
+    public static final int CROP_PHOTO = 2;
+    private Button takePhoto;
+    private ImageView picture;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_takephoto);
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        picture = findViewById(R.id.imageView);
+        takePhoto = findViewById(R.id.button);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takePhotoNoCompress(v);
+                takePhoto();
             }
         });
     }
 
-    public void takePhotoNoCompress(View view) {
+    public void takePhoto() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             String filename = new SimpleDateFormat("yyyyMMdd-HHmmss", Locale.CHINA).format(new Date()) + ".png";
             File file = new File(Environment.getExternalStorageDirectory(), filename);
-            mCurrentPhotoPath = file.getAbsolutePath();
 
-            Uri fileUri;
             if (Build.VERSION.SDK_INT >= 24) {
                 //兼容android7.0 使用共享文件的形式
                 ContentValues contentValues = new ContentValues(1);
@@ -64,30 +70,56 @@ public class TakePhotoActivity extends AppCompatActivity {
                 if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
                     //申请WRITE_EXTERNAL_STORAGE权限
-                    Toast.makeText(this,"请开启存储权限",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, "请开启存储权限", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                fileUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
+                imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues);
 
             } else {
-                fileUri = Uri.fromFile(file);
+                imageUri = Uri.fromFile(file);
             }
 
             List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(takePictureIntent, PackageManager.MATCH_DEFAULT_ONLY);
             for (ResolveInfo resolveInfo : resInfoList) {
                 String packageName = resolveInfo.activityInfo.packageName;
-                grantUriPermission(packageName, fileUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                grantUriPermission(packageName, imageUri, Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
             }
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-            startActivityForResult(takePictureIntent, REQUEST_CODE_TAKE_PHOTO);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(takePictureIntent, PHOTO_REQUEST_CAREMA);
         }
+    }
+
+    /*
+     * 判断sdcard是否被挂载
+     */
+    public static boolean hasSdcard() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_TAKE_PHOTO) {
-            ((ImageView) findViewById(R.id.imageView)).setImageBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath));
+        switch (requestCode) {
+            case PHOTO_REQUEST_CAREMA:
+                if (resultCode == RESULT_OK) {
+                    Intent intent = new Intent("com.android.camera.action.CROP");
+                    intent.setDataAndType(imageUri, "image/*");
+                    intent.putExtra("scale", true);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    startActivityForResult(intent, CROP_PHOTO); // 启动裁剪程序
+                }
+                break;
+            case CROP_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver()
+                                .openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
         }
     }
 }
